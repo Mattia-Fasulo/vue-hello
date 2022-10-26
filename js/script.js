5173ae9b277f94897b24ffef1ec39bf30418abd2
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-10-26 15:39:17 +0200

    add button

--- js/script.js
+++ js/script.js
@@ -17,8 +17,15 @@ const app = createApp({
             altImg: 'picsum_photo',
             classCardBody: 'card-body',
             classCardText: 'card-text text-center',
-            text:'Lorem Picsum'
+            text:'Lorem Picsum',
+            classBtn : "btn btn-dark"
+        }
+    },
+    methods: {
+        changeImg() {
+            this.urlImg = (this.urlImg == 'https://picsum.photos/200/300') ? this.urlImg = 'https://picsum.photos/seed/picsum/200/300' : 'https://picsum.photos/200/300';
         }
     }
 })
 app.mount('#app');
+
